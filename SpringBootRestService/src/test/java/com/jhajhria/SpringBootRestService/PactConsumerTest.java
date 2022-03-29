202302d07a5d662bfe9bb88413a4f309fc312ed8
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-03-29 08:15:48 +0530

    -Empty PactConsumerTest file created with needed annotation -ProviderName defined as well "CoursesCatalogue"

---
+++ SpringBootRestService/src/test/java/com/jhajhria/SpringBootRestService/PactConsumerTest.java
@@ -0,0 +1,14 @@
+package com.jhajhria.SpringBootRestService;
+
+import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
+import au.com.dius.pact.consumer.junit5.PactTestFor;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@ExtendWith(PactConsumerTestExt.class)
+@PactTestFor(providerName = "CoursesCatalog")
+public class PactConsumerTest {
+
+
+}
