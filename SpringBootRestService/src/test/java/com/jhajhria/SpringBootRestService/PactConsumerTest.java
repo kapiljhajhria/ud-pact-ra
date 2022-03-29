9012450168748bed057cd36d01394ccbb2dd80e3
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-03-29 11:55:14 +0530

    modified contract to check only price. As that is the only thing our method is using.

--- SpringBootRestService/src/test/java/com/jhajhria/SpringBootRestService/PactConsumerTest.java
+++ SpringBootRestService/src/test/java/com/jhajhria/SpringBootRestService/PactConsumerTest.java
@@ -22,19 +22,30 @@ import org.springframework.boot.test.context.SpringBootTest;
 @PactTestFor(providerName = "CoursesCatalog")
 public class PactConsumerTest {
 
+//    @Pact(consumer = "CatalogConsumer")// for testAllProductsSum
+//    public V4Pact AllCoursedDetailsPactConfig(PactDslWithProvider builder) throws JsonProcessingException {
+//        //
+//        return builder.given("courses exist").uponReceiving("getting all courses details").
+//                path("/allCourseDetails").method("GET").willRespondWith().status(200).
+//                body(PactDslJsonArray.arrayMinLike(7)
+//                        .stringType("course_name")//value is optional
+//                        .stringType("id")
+//                        .integerType("price",10)
+//                        .stringType("category")
+//                        ).toPact(V4Pact.class);
+//
+//    }
+
     @Pact(consumer = "CatalogConsumer")// for testAllProductsSum
-    public V4Pact AllCoursedDetailsPactConfig(PactDslWithProvider builder) throws JsonProcessingException {
+    public V4Pact AllCoursedDetailsPriceOnlyPactConfig(PactDslWithProvider builder) throws JsonProcessingException {
         //
         return builder.given("courses exist").uponReceiving("getting all courses details").
                 path("/allCourseDetails").method("GET").willRespondWith().status(200).
                 body(PactDslJsonArray.arrayMinLike(2)
-                        .stringType("course_name")//value is optional
-                        .stringType("id")
-                        .integerType("price",12)
-                        .stringType("category")
-                        ).toPact(V4Pact.class);
-
-
+                        .integerType("price",10)
+                ).toPact(V4Pact.class);
+// course_name , id and category are optional for us right now as we are only utilizing price right now.
+// so we can remove those from our test
     }
 
 
@@ -43,9 +54,9 @@ public class PactConsumerTest {
     LibraryController libraryController;
 
     @Test
-    @PactTestFor(pactMethod = "AllCoursedDetailsPactConfig",port = "9999")
+    @PactTestFor(pactMethod = "AllCoursedDetailsPriceOnlyPactConfig",port = "9999")
     public void testAllProductsSum(MockServer mockServer) throws JsonProcessingException {
-        String expectedJson ="{\"booksPrice\":250,\"coursesPrice\":24}";//booksPrice is hardcoded
+        String expectedJson ="{\"booksPrice\":250,\"coursesPrice\":20}";//booksPrice is hardcoded
 
 
         //get allProductPrices depends on CourseApplication service.
