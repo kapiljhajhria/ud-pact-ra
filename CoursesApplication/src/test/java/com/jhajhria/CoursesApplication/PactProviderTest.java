df8d4303720e45495d4ebc076957fa8e760236e3
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-03-29 11:25:36 +0530

    setup and tear down state defined. Even empty state are required to be setup

--- CoursesApplication/src/test/java/com/jhajhria/CoursesApplication/PactProviderTest.java
+++ CoursesApplication/src/test/java/com/jhajhria/CoursesApplication/PactProviderTest.java
@@ -4,6 +4,8 @@ import au.com.dius.pact.provider.junit5.HttpTestTarget;
 import au.com.dius.pact.provider.junit5.PactVerificationContext;
 import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
 import au.com.dius.pact.provider.junitsupport.Provider;
+import au.com.dius.pact.provider.junitsupport.State;
+import au.com.dius.pact.provider.junitsupport.StateChangeAction;
 import au.com.dius.pact.provider.junitsupport.loader.PactFolder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
@@ -26,9 +28,31 @@ public class PactProviderTest {
         context.setTarget(new HttpTestTarget("localhost",port));
     }
 
+    @State(value = "courses exist",action = StateChangeAction.SETUP)
+    public void courseExistSetup(){
+        //setup state
+        
+    }
+
+    @State(value = "courses exist",action = StateChangeAction.TEARDOWN)
+    public void courseExistTearDown(){
+        //teardown state
+
+    }
+
     @TestTemplate // it means test can run multiple times based on how many times this end point test is called by other tests.  pact mock end point
     @ExtendWith(PactVerificationInvocationContextProvider.class)
     public void pactVerificationTest(PactVerificationContext context){
         context.verifyInteraction();
     }
 }
+
+// define port number in the test class
+//define port folder for the test class
+//define setup and tear down state
+// We are checking the interaction between the two services. Interaction when a valid request is made and data exist on server.
+// So setup will be called before each test, this setup will create an object if it is not present.
+// Tear down will then remove that object after each test
+//
+//Test should never fall for invalid request or absence of data
+//
